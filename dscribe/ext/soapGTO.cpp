c92425f02b8e7e718bc44f839784e911551c7963
aki78 <kmorooka78@gmail.com>
2020-08-27 11:15:28 +0300

    done pure tesseral: next test time

--- dscribe/ext/soapGTO.cpp
+++ dscribe/ext/soapGTO.cpp
@@ -307,6 +307,7 @@ void getAlphaBeta(double* aOa, double* bOa, double* alphas, double* betas, int N
 //================================================================
 void getCfactors(double* preCoef, int Asize, double* x,double* x2, double* x4, double* x6, double* x8, double* x10,double* x12,double* x14,double* x16,double* x18, double* y,double* y2, double* y4, double* y6, double* y8, double* y10,double* y12,double* y14,double* y16,double* y18, double* z, double* z2, double* z4, double* z6, double* z8, double* z10,double* z12,double* z14,double* z16,double* z18, double* r2, double* r4, double* r6, double* r8,double* r10,double* r12,double* r14,double* r16,double* r18, double* ReIm2, double* ReIm3, double* ReIm4, double* ReIm5, double* ReIm6, double* ReIm7, double* ReIm8, double* ReIm9,int totalAN, int lMax){
 
+  for (int i = 0; i < Asize; i++) {
     if (lMax > 1){
       /*c20  */  preCoef[        +i] = 1.09254843059208*x[i]*y[i];
       /*c21Re*/  preCoef[totalAN*0+i] = 1.09254843059208*y[i]*z[i];
